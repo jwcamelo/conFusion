dfce40e343148a77da6e8d0011fc29ff235e8f53
Camelo <jhonatan.camelo@capgemini.com>
2022-05-09 16:05:50 -0300

    created dish.service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DishService } from './services/dish.service';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [DishService]
 })
 export class AppRoutingModule { }
